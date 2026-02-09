51d912a6d266b7dd5e3518b1180a89abbdf391c2
JamesClonk <jamesclonk@jamesclonk.ch>
2026-02-09 21:35:14 +0100

    fix jail command

--- kiro-jail.sh
+++ kiro-jail.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source ~/.aws_identity_provider || true
 export KIRO_JAIL=$(basename $(pwd))
 
 podman run -it --rm \
@@ -7,4 +8,4 @@ podman run -it --rm \
 	-v "$(pwd)":/home/kiro/project \
 	-w /home/kiro/project \
 	--name "${KIRO_JAIL}" \
-	kiro-jail:latest kiro-cli
+	kiro-jail:latest /bin/bash -c "kiro-cli login --identity-provider ${AWS_IDENTITY_PROVIDER_URL} --region ${AWS_REGION} --use-device-flow; kiro-cli"
